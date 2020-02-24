1e96155f8625a35538f976fb3d5f7a4b1a11fcf3
bernardw11 <bwang00@stuy.edu>
2020-02-24 10:12:59 -0500

    bruh

--- main.py
+++ main.py
@@ -7,5 +7,17 @@ color = [ 0, 255, 0 ]
 matrix = new_matrix()
 
 
+id = new_matrix()
+id = ident(id)
+
+x = 0
+y = 0
+for i in range(500):
+    x1 = (x * 5)
+    y1 = (y * 3 + 100)
+    add_edge(matrix, x, y, 0, x + x1, y + y1, 0)
+    x = (x + 10) % 500
+    y = (y + 1) % 500
+
 draw_lines( matrix, screen, color )
 display(screen)
--- matrix.py
+++ matrix.py
@@ -12,17 +12,44 @@ import math
 #print the matrix such that it looks like
 #the template in the top comment
 def print_matrix( matrix ):
-    pass
+    line1 = ""
+    line2 = ""
+    line3 = ""
+    line4 = ""
+    for point in matrix:
+        line1 += (str(point[0]) + " ")
+        line2 += (str(point[1]) + " ")
+        line3 += (str(point[2]) + " ")
+        line4 += (str(point[3]) + " ")
+    print(line1)
+    print(line2)
+    print(line3)
+    print(line4)
 
 #turn the paramter matrix into an identity matrix
 #you may assume matrix is square
 def ident( matrix ):
-    pass
+    for x in range(len(matrix)):
+        for y in range(4):
+            if x == y:
+                matrix[x][y] = 1
+            else:
+                matrix[x][y] = 0
 
 #multiply m1 by m2, modifying m2 to be the product
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
-    pass
+    m3 = new_matrix()
+    for x in range(len(m2)):
+        temp = list(m2[x])
+        for y in range(len(m1[0])): #range(4) really
+            sum = 0
+            for i in range(len(m1)):
+                sum += m1[i][y] * m2[x][i]
+            m3[x][y] = sum
+            temp[y] = sum
+        m2[x] = temp
+    return m2
 
 
 
@@ -34,3 +61,18 @@ def new_matrix(rows = 4, cols = 4):
         for r in range( rows ):
             m[c].append( 0 )
     return m
+
+'''
+id = new_matrix()
+print_matrix(id)
+print()
+ident(id)
+print_matrix(id)
+print()
+a = [[3, 4, 2, 1], [4, 3, 6, 1], [2, 3, 4, 1], [6, 8, 9, 1]]
+print_matrix(a)
+
+matrix_mult(id, a)
+print()
+print_matrix(a)
+'''
